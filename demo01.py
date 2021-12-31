d8060bc4e9dc70dc0121e3a3a6a4b63773439bc4
Liuting <18392165884@163.com>
2021-12-31 20:42:22 +0800

    第一次

---
+++ demo01.py
@@ -0,0 +1,27 @@
+"""
+print("你好!")   #字符串
+print(2333)     #整数
+print(2.333)    #小数
+print(True)     #布尔值
+print(())       #元组
+print([])       #数组
+print({})       #字典
+print("First time", 1)
+print("233"+"233") 
+"""
+# a=float(input("请输入:"))
+# b=float(input("请输入:"))
+# print("input获取的内容:",a*b)
+a=input("今天是周几:")
+b=input("现在是周五吗？")
+print("长度是:",len(a)+len(b))
+
+
+# print(type("你好!") )
+# print(type(2333)) 
+# print(type(2.333))   
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
